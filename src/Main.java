ae39832844f30b21aab0b2c17fa6f5fc7202bfff
Agustinicolas <agustinicolas@outlook.com>
2023-08-29 01:37:22 -0300

    implementado menu para crear tragos a voluntad

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,59 @@
 import java.util.concurrent.ForkJoinPool;
+import java.util.LinkedList;
+import Utiles.TecladoIn;
 
 public class Main {
     public static void main(String[] args) {
+        PrepararTrago fabricaTrago;
+        Trago trago;
+        LinkedList<Integer> cuenta = new LinkedList<Integer>();
+        int[] arregloCuenta;
+        int cantidadTragos=0;
+        int opcionElegida = -1;        
+        ForkJoinPool pool = new ForkJoinPool();
+        int precioFinal;
 
-        int[] cuenta=new int[20];
-        int cont=0;
+        //Menu de opciones
+        while (opcionElegida != 0){
+            System.out.println("\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+            System.out.println("\nBARRA: Elegir Trago:");
+            System.out.println("\n1: Fernet");
+            System.out.println("\n2: Daiquiri");
+            System.out.println("\n0: Salir");
+            System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+            opcionElegida = TecladoIn.readLineInt();
+            switch(opcionElegida){
+                case 1:{
+                    fabricaTrago = new PrepararFernetCocaCola();
+                    trago = fabricaTrago.crearTrago("Fernet con coca",600);
+                    trago.iniciarTrago();
+                    trago.ingredients();
+                    trago.preparation();
+                    cuenta.add(600);
+                    cantidadTragos++;
+                }break;
+                case 2:{
+                    fabricaTrago = new PrepararDaiquiri();
+                    trago = fabricaTrago.crearTrago("Daiquiri",1000);
+                    trago.iniciarTrago();
+                    trago.ingredients();
+                    trago.preparation();
+                    cuenta.add(1000);
+                    cantidadTragos++;
+                }break;
+            }
 
-        PrepararTrago fernet = new PrepararFernetCocaCola();
-        Trago f = fernet.crearTrago("Fernet con CocaCola",600);
-        f.iniciarTrago();
-        f.ingredients();
-        f.preparation();
-        cuenta[cont]=f.price;
-        cont++;
-        
-        PrepararTrago fernet2 = new PrepararFernetCocaCola();
-        Trago f2 = fernet2.crearTrago("Fernet con CocaCola",600);
-        f2.iniciarTrago();
-        f2.ingredients();
-        f2.preparation();
-        cuenta[cont]=f2.price;
-        cont++;
-        
-        PrepararTrago fernet3 = new PrepararFernetCocaCola();
-        Trago f3 = fernet3.crearTrago("Fernet con CocaCola",600);
-        f3.iniciarTrago();
-        f3.ingredients();
-        f3.preparation();
-        cuenta[cont]=f3.price;
-        cont++;
-
-        PrepararTrago fernet4 = new PrepararFernetCocaCola();
-        Trago f4 = fernet4.crearTrago("Fernet con CocaCola",600);
-        f4.iniciarTrago();
-        f4.ingredients();
-        f4.preparation();
-        cuenta[cont]=f4.price;
-        cont++;
-
-        PrepararTrago daiquiri = new PrepararDaiquiri();
-        Trago d = daiquiri.crearTrago("Daiquiri",1000);
-        d.iniciarTrago();
-        d.ingredients();
-        d.preparation();
-        cuenta[cont]=d.price;
-        cont++;
-
-        try (ForkJoinPool pool = new ForkJoinPool()) {
-            int result = pool.invoke(new sumarCuentaTragos(cuenta,0,cont));
-            System.out.println("Suma de los targos: " + result);
         }
+
+        //"Conversion" de LinkedList<Integer> a int[]
+        arregloCuenta = new int[cantidadTragos];
+        for(int i=0; i < cantidadTragos; i++){
+            arregloCuenta[i] = (int)cuenta.get(i);
+        }
+
+        precioFinal = pool.invoke(new sumarCuentaTragos(arregloCuenta,0,cantidadTragos));
+        System.out.println("Suma de los targos: " + precioFinal);
+        
 
     }
 
